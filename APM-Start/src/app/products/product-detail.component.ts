d17f80520552e5dc1f5bb471f7776a048febe16f
William Zimmermann <wzimmermann@LGBVINT004.cogismaq.com>
2021-01-19 16:55:37 -0500

    Product Details

--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -1,17 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
+  errorMessage: string;
 
-  constructor() { }
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _productService: ProductService
+  ) {}
 
   ngOnInit(): void {
+    let id = +this._route.snapshot.paramMap.get('id');
+
+    this._productService.getProducts().subscribe({
+      next: (products) => {
+        this.product = products.find((data) => data.productId === id);
+      },
+      error: (err) => (this.errorMessage = err),
+    });
   }
 
+  onBack(): void {
+    this._router.navigate(['/products']);
+  }
 }
